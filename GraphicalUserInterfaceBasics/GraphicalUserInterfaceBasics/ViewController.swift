da28ba044b1d9b6592113818140fcc2d536ea8f2
isakosi <43043457+isakosi@users.noreply.github.com>
2020-10-11 19:02:32 +0600

    Add files via upload

---
+++ GraphicalUserInterfaceBasics/GraphicalUserInterfaceBasics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GraphicalUserInterfaceBasics
+//
+//  Created by Nurtilek on 9/25/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
